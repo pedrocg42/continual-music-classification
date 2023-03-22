900b5fbdeee8f923efacb5f4415fd1dae684916e
pedrocg42 <pedrocorglez@gmail.com>
2023-03-22 22:23:21 +0100

    fix(init): add initis

---
+++ datasets/__init__.py
@@ -0,0 +1,4 @@
+from datasets.gtzan_dataset import GTZANDataset
+from datasets.extraction_pipeline import SimpleMusicPipeline
+
+__all__ = ["GTZANDataset", "SimpleMusicPipeline"]
---
+++ models/__init__.py
@@ -0,0 +1,4 @@
+from models.timm_models import TimmMobileNetV3
+from models.timm_models import TimmMobileViTV2
+
+__all__ = ["TimmMobileNetV3", "TimmMobileViTV2"]
