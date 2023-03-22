1677b09934ec35502a9de906b06129eadb5ea91f
pedrocg42 <pedrocorglez@gmail.com>
2023-03-23 00:45:56 +0100

    fix(ExperimentTracker): accept kwargs

--- experiment_tracker/experiment_tracker.py
+++ experiment_tracker/experiment_tracker.py
@@ -2,5 +2,9 @@ from abc import ABC, abstractmethod
 
 
 class ExperimentTracker(ABC):
-    def __init__(self, experiment_name: str) -> None:
+    def __init__(self, experiment_name: str, **kwargs) -> None:
         self.experiment_name = experiment_name
+
+    @abstractmethod
+    def log_metric(self):
+        pass
