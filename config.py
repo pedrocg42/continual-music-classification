bd8092078aa24a1f6e95700ef117841f79279c91
pedrocg42 <pedrocorglez@gmail.com>
2023-03-23 00:44:25 +0100

    chore(config): add results paths

--- config.py
+++ config.py
@@ -10,3 +10,7 @@ preprocess_device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 logs_path = "results/logs"
+os.makedirs(logs_path, exist_ok=True)
+
+models_path = "results/models"
+os.makedirs(models_path, exist_ok=True)
