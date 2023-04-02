60ce68c7acf7a93b9a9bb54b26ebbddf708fa445
pedrocg42 <pedrocorglez@gmail.com>
2023-04-02 12:39:56 +0200

    chore(init): update transforms init

--- train_data_transforms/__init__.py
+++ train_data_transforms/__init__.py
@@ -1,3 +1,6 @@
+from train_data_transforms.music_genre_classification_transform import (
+    SimpleMusicPipeline,
+)
 from train_data_transforms.train_data_transform import TrainDataTransform
 
-__all__ = ["TrainDataTransform"]
+__all__ = ["TrainDataTransform", "SimpleMusicPipeline"]
