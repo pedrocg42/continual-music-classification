f312cc38065af16c0d092c26862cce7656e50c96
pedrocg42 <pedrocorglez@gmail.com>
2023-04-02 17:07:36 +0200

    chore(execute_experiment): update

--- execute_experiment.py
+++ execute_experiment.py
@@ -9,17 +9,26 @@ def train(experiment_name: str, trainer: Trainer):
     trainer.train(experiment_name)
 
 
-def evaluate(experiment_name: str, evaluator: Evaluator):
-    evaluator.evaluate(experiment_name)
+def evaluate(
+    experiment_name: str,
+    experiment_type: str,
+    experiment_subtype: str,
+    evaluator: Evaluator,
+):
+    evaluator.evaluate(experiment_name, experiment_type, experiment_subtype)
 
 
 @parse_experiment
 def execute_experiment(
     experiment_name: str,
+    experiment_type: str,
+    experiment_subtype: str,
     **experiment,
 ):
     train(experiment_name, **experiment["train"])
-    evaluate(experiment_name, **experiment["evaluate"])
+    evaluate(
+        experiment_name, experiment_type, experiment_subtype, **experiment["evaluate"]
+    )
 
 
 if __name__ == "__main__":
