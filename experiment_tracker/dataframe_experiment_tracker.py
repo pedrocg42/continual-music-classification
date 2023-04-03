f02389505be0b808a4ad966ebdb5bbae125f5b54
pedrocg42 <pedrocorglez@gmail.com>
2023-04-03 22:53:21 +0200

    feat(DataframeExperimentTracker): add log_task_metrics

--- experiment_tracker/dataframe_experiment_tracker.py
+++ experiment_tracker/dataframe_experiment_tracker.py
@@ -36,19 +36,13 @@ class DataframeExperimentTracker(ExperimentTracker):
         train_task_number: int = 0,
         train_task_name: str = "all",
         task_number: int = 0,
-        task_name: str = "all",
     ):
         self.cross_val_id = cross_val_id
         self.train_task_number = train_task_number
         self.train_task_name = train_task_name
         self.task_number = task_number
-        self.task_name = task_name
 
-    def log_metrics(
-        self,
-        metrics: str,
-    ):
-        row = {
+        self.row = {
             "experiment_name": self.experiment_name,
             "experiment_type": self.experiment_type,
             "experiment_subtype": self.experiment_subtype,
@@ -56,14 +50,44 @@ class DataframeExperimentTracker(ExperimentTracker):
             "train_dataset_name": self.dataset_name,
             "train_task_number": self.train_task_number,
             "train_task_name": self.train_task_name,
-            "task_number": self.task_number,
-            "task_name": self.task_name,
         }
+
+    def log_metrics(
+        self,
+        metrics: str,
+        task: str = None,
+    ):
+        row = self.row.copy()
+        row["task_name"] = task
         for metric_name, metric_value in metrics.items():
             row[metric_name] = metric_value.numpy()
         row = pd.Series(row)
 
         self.add_row(row)
+        self.save_results()
+
+    def log_tasks_metrics(
+        self,
+        metrics: dict[str, list[float]],
+        tasks: list[str],
+    ):
+        row = self.row.copy()
+        row["task_name"] = "all"
+        # Extracting average metrics
+        for metric_name, metric_values in metrics.items():
+            row[metric_name] = sum(metric_values) / len(metric_values)
+        row = pd.Series(row)
+        self.add_row(row)
+
+        # Extracting individual metrics
+        for task_number, task_name in enumerate(tasks):
+            row = self.row.copy()
+            row["task_name"] = task_name
+            for metric_name, metric_values in metrics.items():
+                row[metric_name] = metric_values[task_number]
+            row = pd.Series(row)
+            self.add_row(row)
+
         self.save_results()
 
     def add_row(self, row: dict):
