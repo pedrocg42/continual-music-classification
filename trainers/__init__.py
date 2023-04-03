93080efcfe30c1dfb7d9af1f32caf89a93c87e1a
pedrocg42 <pedrocorglez@gmail.com>
2023-04-03 23:51:45 +0200

    feat(Continual LearningTrainer): add

--- trainers/__init__.py
+++ trainers/__init__.py
@@ -1,3 +1,4 @@
 from trainers.trainer import Trainer
+from trainers.continual_learning_trainer import ContinualLearningTrainer
 
-__all__ = ["Trainer"]
+__all__ = ["Trainer", "ContinualLearningTrainer"]
