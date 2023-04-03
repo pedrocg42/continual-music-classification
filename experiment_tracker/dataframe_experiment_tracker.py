5529917da269ef64c7379e52f28edcd35e6485a7
pedrocg42 <pedrocorglez@gmail.com>
2023-04-03 21:23:30 +0200

    feat(DataframeExperimentTracker): update to track cv and task

--- experiment_tracker/dataframe_experiment_tracker.py
+++ experiment_tracker/dataframe_experiment_tracker.py
@@ -30,23 +30,34 @@ class DataframeExperimentTracker(ExperimentTracker):
         self.experiment_subtype = experiment_subtype
         self.dataset_name = dataset_name
 
+    def configure_task(
+        self,
+        cross_val_id: int,
+        train_task_number: int = 0,
+        train_task_name: str = "all",
+        task_number: int = 0,
+        task_name: str = "all",
+    ):
+        self.cross_val_id = cross_val_id
+        self.train_task_number = train_task_number
+        self.train_task_name = train_task_name
+        self.task_number = task_number
+        self.task_name = task_name
+
     def log_metrics(
         self,
         metrics: str,
-        cv_fold: int = 0,
-        task_number: int = 0,
-        task_name: str = "",
-        train_subdataset_name: str = "",
     ):
         row = {
             "experiment_name": self.experiment_name,
             "experiment_type": self.experiment_type,
             "experiment_subtype": self.experiment_subtype,
+            "cv_fold": self.cross_val_id,
             "train_dataset_name": self.dataset_name,
-            "train_subdataset_name": train_subdataset_name,
-            "cv_fold": cv_fold,
-            "task_number": task_number,
-            "task_name": task_name,
+            "train_task_number": self.train_task_number,
+            "train_task_name": self.train_task_name,
+            "task_number": self.task_number,
+            "task_name": self.task_name,
         }
         for metric_name, metric_value in metrics.items():
             row[metric_name] = metric_value.numpy()
