3a4eb34deb475297ca898feb074a93d22167ad0a
pedrocg42 <pedrocorglez@gmail.com>
2023-04-06 20:53:43 +0200

    feat(CLEvaluator): create new evaluator for tasks

--- evaluators/__init__.py
+++ evaluators/__init__.py
@@ -3,5 +3,13 @@ from evaluators.tasks_evaluator import TasksEvaluator
 from evaluators.continual_learning_tasks_evaluator import (
     ContinualLearningTasksEvaluator,
 )
+from evaluators.continual_learning_tasks_evaluator_v2 import (
+    ContinualLearningTasksEvaluatorV2,
+)
 
-__all__ = ["Evaluator", "TasksEvaluator", "ContinualLearningTasksEvaluator"]
+__all__ = [
+    "Evaluator",
+    "TasksEvaluator",
+    "ContinualLearningTasksEvaluator",
+    "ContinualLearningTasksEvaluatorV2",
+]
