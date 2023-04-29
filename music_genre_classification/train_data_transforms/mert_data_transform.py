eb444991d3cb0babf5641de2c1eb27ec4bf9ab27
pedrocg42 <pedrocorglez@gmail.com>
2023-04-29 14:39:54 -0400

    feat(MertDataTransform): add new transform

---
+++ music_genre_classification/train_data_transforms/mert_data_transform.py
@@ -0,0 +1,30 @@
+import torch
+import torchaudio.transforms as T
+from music_genre_classification.train_data_transforms.train_data_transform import (
+    TrainDataTransform,
+)
+from transformers import Wav2Vec2FeatureExtractor
+
+
+class MertDataTransform(TrainDataTransform):
+    def __init__(
+        self,
+        input_sample_rate: int = 22050,
+        output_sample_rate: int = 24000,
+    ):
+        super().__init__()
+
+        self.input_sample_rate = input_sample_rate
+        self.output_sample_rate = output_sample_rate
+        self.resampler = T.Resample(input_sample_rate, output_sample_rate)
+        self.processor = Wav2Vec2FeatureExtractor.from_pretrained(
+            "m-a-p/MERT-v1-95M", trust_remote_code=True
+        )
+
+    def transform(self, inputs: torch.Tensor, **kwargs) -> torch.Tensor:
+        inputs = self.resampler(inputs)
+        inputs = {"input_values": inputs, "attention_mask": torch.ones(len(inputs))}
+        return inputs
+
+    def to(self, *args, **kwargs) -> None:
+        pass
