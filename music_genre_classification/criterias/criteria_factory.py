3b1afdd79c16b70206ae8e400db04d6546b26546
pedrocg42 <pedrocorglez@gmail.com>
2023-04-29 14:31:02 -0400

    feat(CriteriaFactory): create factory

---
+++ music_genre_classification/criterias/criteria_factory.py
@@ -0,0 +1,17 @@
+from music_genre_classification.criterias.criteria import Criteria
+
+
+class CriteriaFactory:
+    """
+    Factory class for creating criterias.
+    """
+
+    def build(config: dict) -> Criteria:
+        if config["name"] == "TorchCrossEntropyCriteria":
+            from music_genre_classification.criterias import (
+                TorchCrossEntropyCriteria,
+            )
+
+            return TorchCrossEntropyCriteria(**config.get("args", {}))
+        else:
+            raise ValueError(f"Unknown TrainDataSource type: {config['name']}")
