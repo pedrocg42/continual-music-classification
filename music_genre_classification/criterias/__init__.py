0874547559fa94c7940a278a9f966f73fc28e0ee
pedrocg42 <pedrocorglez@gmail.com>
2023-04-29 14:40:25 -0400

    feat(CriteriaFactory): create factory

--- music_genre_classification/criterias/__init__.py
+++ music_genre_classification/criterias/__init__.py
@@ -2,5 +2,6 @@ from music_genre_classification.criterias.criteria import Criteria
 from music_genre_classification.criterias.torch_cross_entropy_criteria import (
     TorchCrossEntropyCriteria,
 )
+from music_genre_classification.criterias.criteria_factory import CriteriaFactory
 
-__all__ = ["Criteria", "TorchCrossEntropyCriteria"]
+__all__ = ["CriteriaFactory", "Criteria", "TorchCrossEntropyCriteria"]
