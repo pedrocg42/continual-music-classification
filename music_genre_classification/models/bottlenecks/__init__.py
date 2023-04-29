9e21d9e31a597fa74b39aad334fb1adccf0f7d63
pedrocg42 <pedrocorglez@gmail.com>
2023-04-29 14:33:20 -0400

    feat(BottleneckFactory): create factory

--- music_genre_classification/models/bottlenecks/__init__.py
+++ music_genre_classification/models/bottlenecks/__init__.py
@@ -1,4 +1,11 @@
-from models.bottlenecks.discrete_key_value_bottleneck import DKVB
-from models.bottlenecks.vector_quantizer import VectorQuantizer
+from music_genre_classification.models.bottlenecks.discrete_key_value_bottleneck import (
+    DKVB,
+)
+from music_genre_classification.models.bottlenecks.vector_quantizer import (
+    VectorQuantizer,
+)
+from music_genre_classification.models.bottlenecks.bottleneck_factory import (
+    BottleneckFactory,
+)
 
-__all__ = ["VectorQuantizer", "DKVB"]
+__all__ = ["BottleneckFactory", "VectorQuantizer", "DKVB"]
---
+++ music_genre_classification/models/bottlenecks/bottleneck_factory.py
@@ -0,0 +1,23 @@
+from torch.nn import Module
+
+
+class BottleneckFactory:
+    """
+    Factory class for creating encoders.
+    """
+
+    def build(config: dict) -> Module:
+        if config["name"] == "DKVB":
+            from music_genre_classification.models.bottlenecks import (
+                DKVB,
+            )
+
+            return DKVB(**config.get("args", {}))
+        elif config["name"] == "VectorQuantizer":
+            from music_genre_classification.models.bottlenecks import (
+                VectorQuantizer,
+            )
+
+            return VectorQuantizer(**config.get("args", {}))
+        else:
+            raise ValueError(f"Unknown looper type: {config['name']}")
