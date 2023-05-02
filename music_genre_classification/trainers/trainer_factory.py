f41da2b9b9e3d321434135fd2bd6433ae754aee7
pedrocg42 <pedrocorglez@gmail.com>
2023-05-02 19:26:57 -0400

    chore(TrainFactory): update

--- music_genre_classification/trainers/trainer_factory.py
+++ music_genre_classification/trainers/trainer_factory.py
@@ -1,19 +1,21 @@
 from music_genre_classification.trainers import Trainer
+
+
 class TrainerFactory:
     """
     Factory class for creating trainers.
     """
 
     @staticmethod
-    def build(trainer_name, args) -> Trainer:
+    def build(config: dict) -> Trainer:
         """
         Create trainer.
         :param trainer_name: Name of the trainer.
         :param args: Arguments for the trainer.
         """
-        if trainer_name == "ContinualLearningTrainer":
+        if config["name"] == "ContinualLearningTrainer":
             from music_genre_classification.trainers import ContinualLearningTrainer
 
-            return ContinualLearningTrainer(**args)
+            return ContinualLearningTrainer(**config["args"])
         else:
-            raise Exception("No trainer named %s" % trainer_name)
+            raise Exception("No trainer named %s" % config["name"])
