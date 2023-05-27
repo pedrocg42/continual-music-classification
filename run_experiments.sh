11a5dcf6bd36cf79caabe0b6b7d6a8bc1ac3aa92
pedrocg42 <pcordoba@alicebiometrics.com>
2023-05-27 14:03:55 +0000

    chore(run_experiments): add bash version

---
+++ run_experiments.sh
@@ -0,0 +1,14 @@
+#  Joint training
+
+python execute_experiment.py --experiment Mert95m_joint_gtzan_all
+python execute_experiment.py --experiment Mert95mVq_joint_gtzan_all
+python execute_experiment.py --experiment Mert95mDkvb_joint_gtzan_all
+
+#  Scenario 1
+
+python execute_experiment.py --experiment Mert95m_cl_gtzan_scenario1
+python execute_experiment.py --experiment Mert95mVq_cl_gtzan_scenario1
+python execute_experiment.py --experiment Mert95mDkvb_cl_gtzan_scenario1
+
+# Turn off VM (comment if not needed)
+sudo shutdown
