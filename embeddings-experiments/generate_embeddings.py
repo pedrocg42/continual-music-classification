38ebb7493b71d8fe8367313e9b7509b6ac068c49
pedrocg42 <pedrocorglez@gmail.com>
2024-02-05 22:00:07 -0500

    chore(generate_embeddings): improve script

--- embeddings-experiments/generate_embeddings.py
+++ embeddings-experiments/generate_embeddings.py
@@ -16,20 +16,41 @@ from tqdm import tqdm
 torch.set_grad_enabled(False)
 
 
-def generate_embeddings(
+def generate_embeddings():
+
+    dataset_configs = {
+        "gtzan": {
+            "data_config": dict(name="GtzanDataSource"),
+            "input_sample_rate": 22050,
+        },
+        "vocalset-singer": {
+            "data_config": dict(name="VocalSetSingerDataSource"),
+            "input_sample_rate": 44100,
+        },
+        "vocalset-tech": {
+            "data_config": dict(name="VocalSetTechDataSource"),
+            "input_sample_rate": 44100,
+        },
+        "nsynth": {
+            "data_config": dict(name="NSynthInstrumentTechDataSource"),
+            "input_sample_rate": 16000,
+        },
+    }
+
+    for output_name, kwargs in dataset_configs.items():
+        logger.info(f"Starting embedding extraction of dataset: {output_name}")
+        generate_dataset_embeddings(output_name=output_name, **kwargs)
+
+
+def generate_dataset_embeddings(
     encoder_config: dict = dict(name="MertEncoder"),
-    # data_config: dict = dict(name="GtzanDataSource"),
-    # input_sample_rate: int = 22050, # GTZAN
-    # data_config: dict = dict(name="VocalSetSingerDataSource"),
-    # data_config: dict = dict(name="VocalSetTechDataSource"),
-    # input_sample_rate: int = 44100,  # VocalSet
-    data_config: dict = dict(
-        name="NSynthInstrumentTechDataSource", args=dict(num_items_per_class=-1)
-    ),
-    input_sample_rate: int = 16000,  # NSynth
-    output_name: str = "nsynth",
+    data_config: dict = dict(name="GtzanDataSource"),
+    input_sample_rate: int = 22050,
+    output_name: str = "gtzan",
 ):
-    embedding_model = TorchEmbeddingModel(encoder=encoder_config).to(config.device)
+    embedding_model = (
+        TorchEmbeddingModel(encoder=encoder_config).to(config.device).eval()
+    )
     data_transform = MertDataTransform(input_sample_rate=input_sample_rate).to(
         config.device
     )
@@ -37,7 +58,11 @@ def generate_embeddings(
     for split in ["train", "val", "test"]:
         logger.info(f"Starting embedding extraction of split: {split}")
 
-        data_config["args"].update(dict(split=split, is_eval=True))
+        if output_name == "nsynth" and split == "train":
+            data_config["args"] = dict(split=split, is_eval=True, num_items_per_class=5000)
+        else:
+            data_config["args"] = dict(split=split, is_eval=True)
+
         data_source = TrainDataSourceFactory().build(config=data_config)
         dataset = data_source.get_dataset()
 
@@ -51,12 +76,10 @@ def generate_embeddings(
             chunks_transformed = data_transform(chunks)
             chunks_embeddings = embedding_model.forward_features(chunks_transformed)
 
-            embeddings.append(chunks_embeddings.cpu().numpy())
-            labels += [
-                # data_source.index_to_genre[index] for index in chunks_labels.numpy()
-                data_source.index_to_instrument[index]
-                for index in chunks_labels.numpy()
-            ]
+            embeddings.append(
+                chunks_embeddings.cpu().numpy().mean(axis=0, keepdims=True)
+            )
+            labels.append(chunks_labels[0].numpy())
 
         embeddings = np.concatenate(embeddings)
         labels = np.array(labels)
