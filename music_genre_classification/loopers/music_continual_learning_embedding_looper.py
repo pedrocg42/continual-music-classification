fix(TorchEmbeddingModel): avoid averaging embeddings for CLMR

music_genre_classification/loopers/music_continual_learning_embedding_looper.py
@@ -27,7 +27,7 @@ class MusicContinualLearningEmbeddingLooper(ABC):
         model: nn.Module,
         data_loader: DataLoader | Dataset,
         data_transform: nn.Module,
-    ) -> dict():
+    ) -> dict[str, torch.Tensor]:
         logger.info(f"Training epoch")
         model.prepare_train()
         results_epoch = []
@@ -60,7 +60,6 @@ class MusicContinualLearningEmbeddingLooper(ABC):
         data_transform: nn.Module,
     ):
         inputs = inputs.to(config.device, non_blocking=True)
-        labels = labels.to(config.device, non_blocking=True)
 
         # Inference
         transformed = data_transform(inputs, augment=True)

music_genre_classification/models/embedding_model.py
@@ -8,9 +8,11 @@ from music_genre_classification.models.encoders import EncoderFactory
 
 
 class TorchEmbeddingModel(nn.Module):
-    def __init__(self, encoder: dict, **kwargs):
+    def __init__(self, encoder: dict, average_hidden: bool = True, **kwargs):
         super().__init__()
         self.encoder = EncoderFactory.build(encoder)
+        self.average_hidden = average_hidden
+
         self.register_buffer(
             "reference_embeddings", torch.zeros(0, self.encoder.output_size)
         )
@@ -24,7 +26,8 @@ class TorchEmbeddingModel(nn.Module):
 
     def forward_features(self, inputs: torch.Tensor) -> torch.Tensor:
         embeddings = self.encoder(inputs)
-        embeddings = torch.mean(embeddings, dim=1)
+        if self.average_hidden:
+            embeddings = torch.mean(embeddings, dim=1)
         return embeddings
 
     def match_embeddings(self, embeddings: torch.Tensor) -> torch.Tensor: